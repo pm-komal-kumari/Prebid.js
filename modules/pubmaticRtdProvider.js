cdb688efa9a1e6dd5a75b0d436f032b90ad4fefb
Komal Kumari <komalkumari@Komals-MacBook-Pro-L1834.local>
2024-10-25 11:57:15 +0530

    Time of day : update PubMaticRTDProvider

--- modules/pubmaticRtdProvider.js
+++ modules/pubmaticRtdProvider.js
@@ -1,5 +1,5 @@
 import { submodule } from '../src/hook.js';
-import { logInfo, logError } from '../src/utils.js';
+import { logInfo, logError, mergeDeep, isEmptyStr } from '../src/utils.js';
 import { getGlobal } from '../src/prebidGlobal.js';
 import {config as conf} from '../src/config.js';
 
@@ -9,6 +9,51 @@ import {config as conf} from '../src/config.js';
 
 const REAL_TIME_MODULE = 'realTimeData';
 const SUBMODULE_NAME = 'pubmatic';
+const LOG_PRE_FIX = 'PubMatic-Rtd-Provider: ';
+let calculatedTimeOfDay = 'morning';
+
+function getDeviceTypeFromUserAgent(userAgent) {
+  // Normalize user agent string to lowercase for easier matching
+  const ua = userAgent.toLowerCase();
+  // Check for mobile devices
+  if (/mobile|iphone|ipod|android.*mobile|blackberry|windows phone/.test(ua)) {
+    return 'mobile';
+  }
+  // Check for tablets
+  if (/tablet|ipad|android(?!.*mobile)/.test(ua)) {
+    return 'tablet';
+  }
+  // Default to desktop if neither mobile nor tablet matches
+  return 'desktop';
+}
+function deviceTypes() {
+  let deviceType = getDeviceTypeFromUserAgent(navigator.userAgent);
+  console.log('Pubmatic rtd provider -> In deviceType fn -> deviceType -> ',deviceType);
+  if(deviceType == 'mobile')
+      return 'mobile'
+  else if (deviceType == 'tablet')
+      return 'tablet'
+  else if (deviceType == 'desktop')
+      return 'desktop'
+}
+
+function timeOfDay(){
+  return calculatedTimeOfDay;
+}
+
+function calculateTimeOfDay() {
+  const currentHour = new Date().getHours();  // Get the current hour (0-23)
+
+  if (currentHour >= 5 && currentHour < 12) {
+    calculatedTimeOfDay = 'morning';
+  } else if (currentHour >= 12 && currentHour < 17) {
+    calculatedTimeOfDay = 'afternoon';
+  } else if (currentHour >= 17 && currentHour < 19) {
+    calculatedTimeOfDay = 'evening';
+  } else {
+    calculatedTimeOfDay = 'night';
+  }
+}
 
 /**
  * Initialize the Adagio RTD Module.
@@ -17,7 +62,32 @@ const SUBMODULE_NAME = 'pubmatic';
  * @returns {boolean}
  */
 function init(config, _userConsent) {  
+  const publisherId = config.params?.publisherId;
+  const profileId = config.params?.profileId;
+
+  if (publisherId || isEmptyStr(publisherId)) {
+    logError(LOG_PRE_FIX + 'Missing publisherId.');
+    return false;
+  }
+
+  if (profileId || isEmptyStr(profileId)) {
+    logError(LOG_PRE_FIX + 'Missing profileId.');
+    return false;
+  }
+
+  calculateTimeOfDay();
+  const globalConfig = conf.getConfig();
   console.log('Pubmatic rtd provider -> In init fn -> globalConfig.floors -> ', globalConfig.floors);
+ 
+  conf.mergeConfig({
+      floors: {
+        additionalSchemaFields: {
+          deviceType : deviceTypes,
+          timeOfDay: timeOfDay
+        }
+      }
+    });
+    return true;
 }
 
 /**
@@ -28,7 +98,18 @@ function init(config, _userConsent) {
  */
 function getBidRequestData(reqBidsConfigObj, callback, config, userConsent) {
   console.log('Pubmatic rtd provider -> In getBidRequestData fn -> reqBidsConfigObj', reqBidsConfigObj);
-  console.log('Pubmatic rtd provider -> In getBidRequestData fn -> config', config);
+
+  const Ortb2 = {
+    user : {
+      ext : {
+        name : 'komal',
+        deviceTypes : deviceTypes(),
+        timeOfDay : timeOfDay()
+      }
+    }
+  }
+
+  mergeDeep(reqBidsConfigObj.ortb2Fragments.global, Ortb2);
   callback();
 }    
 
@@ -44,7 +125,6 @@ export const pubmaticSubmodule = {
   };
   
   function registerSubModule() {
-    console.log('Pubmatic rtd provider -> In registerSubModule fn');
     submodule(REAL_TIME_MODULE, pubmaticSubmodule);
   }
   
